047589b9bf26089b3331b42c44f4ae736421f126
Just-Hussain <1just.hussain@gmail.com>
2021-07-13 13:18:34 +0300

    set lang=ts

--- pages/login.vue
+++ pages/login.vue
@@ -11,15 +11,14 @@
 	</div>
 </template>
 
-<script>
+<script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 
 @Component({ name: 'login', auth: 'guest' })
 export default class extends Vue {
 	async login() {
 		try {
-			const response = await this.$auth.loginWith('keycloak')
-			console.log(response)
+			await this.$auth.loginWith('keycloak')
 		} catch (err) {
 			console.log(err)
 		}
