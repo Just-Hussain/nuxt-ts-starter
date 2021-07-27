7eca4402d9a4b31af3b70bdd33529d2ec28654d4
Just-Hussain <1just.hussain@gmail.com>
2021-07-27 18:05:51 +0300

    fix sitemap routes

--- utils/configs/sitemap.config.ts
+++ utils/configs/sitemap.config.ts
@@ -1,11 +1,12 @@
 // https://sitemap.nuxtjs.org/usage/sitemap-options
 
 export default {
-	// Include dynamic routes here.
-	// routes: [],
-	// i18n: true,
 	hostname:
 		process.env.NUXT_ENV_VERCEL_URL ||
 		process.env.VERCEL_URL ||
 		process.env.BASE_URL,
+
+	// Include dynamic routes here.
+	routes: ['/test'],
+	// i18n: true,
 };
