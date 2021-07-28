4836b3121ddcd919cf3bf632ca4064be49b687e4
Just-Hussain <1just.hussain@gmail.com>
2021-07-28 12:21:30 +0300

    update robots sitemap url

--- utils/configs/robots.config.ts
+++ utils/configs/robots.config.ts
@@ -1,7 +1,7 @@
 export default [
 	{
 		Sitemap:
-			`${process.env.NUXT_ENV_VERCEL_URL}/sitemap.xml` ||
+			// `${process.env.NUXT_ENV_VERCEL_URL}/sitemap.xml` ||
 			`${process.env.VERCEL_URL}/sitemap.xml` ||
 			`${process.env.BASE_URL}/sitemap.xml`,
 	},
