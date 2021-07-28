27ca49729df674105ddaacc071392ab9a65c962f
Just-Hussain <1just.hussain@gmail.com>
2021-07-28 12:15:52 +0300

    add robots

---
+++ utils/configs/robots.config.ts
@@ -0,0 +1,8 @@
+export default [
+	{
+		Sitemap:
+			`${process.env.NUXT_ENV_VERCEL_URL}/sitemap.xml` ||
+			`${process.env.VERCEL_URL}/sitemap.xml` ||
+			`${process.env.BASE_URL}/sitemap.xml`,
+	},
+];
