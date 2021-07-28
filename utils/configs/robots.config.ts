92b423da4b7e7fa133458eddeee98a87bd5856aa
Just-Hussain <1just.hussain@gmail.com>
2021-07-28 15:53:13 +0300

    fix robot sitemap

--- utils/configs/robots.config.ts
+++ utils/configs/robots.config.ts
@@ -3,6 +3,6 @@
 // Typescript support is weird for this one, so it is not used.
 export default [
 	{
-		Sitemap: `${process.env.BASE_URL}/sitemap.xml`,
+		Sitemap: `https://${process.env.BASE_URL}/sitemap.xml`,
 	},
 ];
