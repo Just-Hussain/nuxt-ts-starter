d7c8b0046fcb9ef58285480debcbbeead6778bf7
Just-Hussain <1just.hussain@gmail.com>
2021-07-28 14:23:54 +0300

    sitemap i18n

--- utils/configs/sitemap.config.ts
+++ utils/configs/sitemap.config.ts
@@ -5,4 +5,8 @@ export default {
 
 	// Include dynamic routes here.
 	routes: [],
+
+	// With i18n enabled, viewing the sitemap.xml in the browser does not render well.
+	// View page source to see sitemap.
+	i18n: true,
 };
