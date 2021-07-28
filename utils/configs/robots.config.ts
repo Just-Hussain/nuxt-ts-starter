0879bb8a7548930a9aa32e5d3995e722cc50895c
Just-Hussain <1just.hussain@gmail.com>
2021-07-28 15:52:44 +0300

    fix robot sitemap

--- utils/configs/robots.config.ts
+++ utils/configs/robots.config.ts
@@ -2,7 +2,7 @@ export default [
 	{
 		Sitemap:
 			// `${process.env.NUXT_ENV_VERCEL_URL}/sitemap.xml` ||
-			`${process.env.VERCEL_URL}/sitemap.xml` ||
-			`${process.env.BASE_URL}/sitemap.xml`,
+			`https://${process.env.VERCEL_URL}/sitemap.xml` ||
+			`https://${process.env.BASE_URL}/sitemap.xml`,
 	},
 ];
